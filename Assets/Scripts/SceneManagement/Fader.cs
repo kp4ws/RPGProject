2f2a67ffba153a6f888ef1b4893b1559b836fd3d
kp4ws <kentpawson123@gmail.com>
2021-11-08 21:24:37 -0700

    Finished scene management

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        private CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                //moving alpha towards 1
+                canvasGroup.alpha += Time.deltaTime / time;
+
+                //Waits 1 frame
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0) 
+            {
+                //moving alpha towards 0
+                canvasGroup.alpha -= Time.deltaTime / time;
+
+                //Waits 1 frame
+                yield return null;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.AI;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        private enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
+        [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private DestinationIdentifier destination;
+        [SerializeField] private float fadeOutTime = 0.5f;
+        [SerializeField] private float fadeInTime = 1f;
+        [SerializeField] private float fadeWaitTime = 0.5f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            if(sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load is not set");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            Fader fader = FindObjectOfType<Fader>();
+
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+            
+            //TODO bug fix
+            //
+            //  If previous portal doesn't get destroyed before walking into next portal, you get stuck in infinite void
+            //
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+    }
+}
